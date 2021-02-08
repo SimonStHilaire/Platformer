18930a29f3b55a9b2b0ddc48e1553273364ea253
SimonStHilaire <simon.sthilaire@gmail.com>
2021-02-08 13:52:21 -0500

    Add GameSettings and upgrade to 2019.4.19

---
+++ Assets/ScriptableObjects/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "ScriptableObjects/NewGameSettings", order = 1)]
+public class GameSettings : ScriptableObject
+{
+    public float PlayerProjectilesSpeed;
+    public bool DebugDraw;
+}
