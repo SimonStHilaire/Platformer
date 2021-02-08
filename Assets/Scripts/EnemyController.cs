44406fd36abab427b083eac2f16079f22e816f38
SimonStHilaire <simon.sthilaire@gmail.com>
2021-02-08 01:23:02 -0500

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class EnemyController : MonoBehaviour
+{
+    [Header("Gameplay")]
+    public float Speed;
+    public bool CanDrop;
+
+    [Header("Settings")]
+    public LayerMask EnvLayerMask;
+
+    private Rigidbody2D Body;
+    private BoxCollider2D Box;
+
+    private int Direction = 1;
+
+    void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+        Box = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Body.position += Vector2.right * Time.deltaTime * Direction * Speed;
+    }
+
+    void Update()
+    {
+        bool grounded = Physics2D.Raycast(transform.position, Vector2.down, Box.size.y + 0.05f, EnvLayerMask);
+
+        if (Direction > 0)
+        {
+            //Look for env obstacle on my right
+            RaycastHit2D castResult = Physics2D.Raycast(transform.position, Vector2.right, Box.size.x + 0.05f, EnvLayerMask);
+
+            if (castResult)
+            {
+                Direction *= -1;
+            }
+            else if(grounded && !CanDrop)
+            {
+                //Look for hole on my right
+                castResult = Physics2D.Raycast(transform.position + new Vector3(Box.size.x, 0f, 0f), Vector2.down, Box.size.y + 0.05f, EnvLayerMask);
+
+                if(!castResult)
+                {
+                    Direction *= -1;
+                }
+            }
+        }
+        else
+        {
+            //Look for env obstacle on my left
+            RaycastHit2D castResult = Physics2D.Raycast(transform.position, Vector2.left, Box.size.x + 0.05f, EnvLayerMask);
+
+            if (castResult)
+            {
+                Direction *= -1;
+            }
+            else if (grounded && !CanDrop)
+            {
+                //Look for hole on my left
+                castResult = Physics2D.Raycast(transform.position - new Vector3(Box.size.x, 0f, 0f), Vector2.down, Box.size.y + 0.05f, EnvLayerMask);
+
+                if (!castResult)
+                {
+                    Direction *= -1;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    
+    public float JumpForce;
+    public float MoveSpeed;
+
+    Rigidbody2D Body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<EnemyController>())
+            Debug.Log("Player touched by an enemy!");
+    }
+}
