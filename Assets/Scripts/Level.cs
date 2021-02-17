bc4e11aad62fef4bf87260e1a23c74826ab4eac6
SimonStHilaire <simon.sthilaire@gmail.com>
2021-02-17 14:04:41 -0500

    Split level and manager

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    Config LevelConfig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadConfig();
+    }
+
+    void ReadConfig()
+    {
+        string jsonConfig = File.ReadAllText("config.json");
+
+        LevelConfig = JsonUtility.FromJson<Config>(jsonConfig);
+
+        Physics2D.gravity *= LevelConfig.GravityModifier;
+    }
+
+    void GenerateConfig()
+    {
+        Config levelConfig = new Config();
+
+        levelConfig.GravityModifier = 0.5f;
+        levelConfig.EnemyRespawnInterval = 1f;
+
+        levelConfig.Enemies = new List<EnemyConfig>(10);
+
+        for (int i = 0; i < 10; ++i)
+        {
+            EnemyConfig enemy = new EnemyConfig();
+            enemy.CanDrop = false;
+            enemy.Speed = Random.Range(10f, 15f);
+            enemy.SpawnPosition = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f);
+
+            levelConfig.Enemies.Add(enemy);
+        }
+
+        string jsonConfig = JsonUtility.ToJson(levelConfig);
+
+        File.WriteAllText("config.json", jsonConfig);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/configs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+[Serializable]
+public class EnemyConfig
+{
+    public Vector3 SpawnPosition;
+    public float Speed;
+    public bool CanDrop;
+}
+
+[Serializable]
+public class Config
+{
+    public float GravityModifier;
+    public float EnemyRespawnInterval;
+
+    public List<EnemyConfig> Enemies;
+}
