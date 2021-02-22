0ce292ffbc6e44a55da61d0b32cc94c0f7a595ea
Phillippe Canuel <phillippe@hotmail.ca>
2021-02-22 14:36:00 -0500

    add soundController

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+
+    public AudioClip Jump;
+    public AudioClip Shoot;
+    public AudioClip Death;
+
+    private AudioClip getAudio(string name)
+    {
+        switch (name)
+        {
+            case "Jump":
+                return Jump;
+            case "Shoot":
+                return Shoot;
+            case "Death":
+                return Death;
+        }
+        return null;
+    }
+
+    public void playAudio(string audioName, AudioSource audioSource)
+    {
+        audioSource.clip = getAudio(audioName);
+        audioSource.Play();
+    }
+
+}
