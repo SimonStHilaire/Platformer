58e7994d7f60038e274e6c9729be504dd8bf071e
Phillippe Canuel <phillippe@hotmail.ca>
2021-02-24 13:41:20 -0500

    FX Life Time

---
+++ Assets/Scripts/FXLifeTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXLifeTime : MonoBehaviour
+{
+    private float Life;
+
+    private void Start()
+    {
+        Life = gameObject.GetComponent<ParticleSystem>().main.duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Life -= Time.deltaTime;
+
+        if (Life < 0)
+            Destroy(gameObject);
+    }
+}
