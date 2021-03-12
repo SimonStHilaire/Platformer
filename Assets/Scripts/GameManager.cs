84110ebc7173b2cd3e92980780e51bfc34217716
SimonStHilaire <simon.sthilaire@gmail.com>
2021-03-12 13:17:05 -0500

    Fix since we have dynamic levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -69,6 +69,7 @@ public class GameManager : SceneSingleton<GameManager>
             else
             {
                 CurrentLevelIndex = 1;
+                nextSceneName = LEVEL_NAME + CurrentLevelIndex.ToString();
             }
         }
 
