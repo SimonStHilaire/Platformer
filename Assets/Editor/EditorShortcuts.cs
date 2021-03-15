01d39cdc45976129b214bce5e69845951dc9defa
SimonStHilaire <simon.sthilaire@gmail.com>
2021-03-15 14:35:30 -0400

    Script editeur
    Shortcut -> GoToMainScene

---
+++ Assets/Editor/EditorShortcuts.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class EditorShortcuts
+{
+    [MenuItem("Shortcuts/Go To Main Scene %F1")]
+    public static void GoToMainScene()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Scenes/main.unity");
+    }
+}
